13a7e6e2e5e5aebd334f7bed812f125b52c13af7
Atiq-coder <coderatiq@gmail.com>
2021-10-19 01:45:50 +0600

    responsive nav

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
+
+
 
 const Header = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand>Rahman Health Center</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link><Link className="link" to="/home">Home</Link></Nav.Link>
-                        <Nav.Link><Link className="link" to="/services">Services</Link></Nav.Link>
-                        <Nav.Link><Link className="link" to="/doctors">Doctors</Link></Nav.Link>
-                        <Nav.Link><Link className="link" to="/appointment">Appointment</Link></Nav.Link>
-                        <Nav.Link><Link className="link" to="/contact">Contact Us</Link></Nav.Link>
-                    </Nav>
-                </Container>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand>Rahman Health Center</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Container>
+                        <Nav className="me-auto">
+                            <Nav.Link><Link className="link" to="/home">Home</Link></Nav.Link>
+                            <Nav.Link><Link className="link" to="/services">Services</Link></Nav.Link>
+                            <Nav.Link><Link className="link" to="/doctors">Doctors</Link></Nav.Link>
+                            <Nav.Link><Link className="link" to="/appointment">Appointment</Link></Nav.Link>
+                            <Nav.Link><Link className="link" to="/contact">Contact Us</Link></Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar.Collapse>
             </Navbar>
-        </div>
+        </div >
     );
 };
 
